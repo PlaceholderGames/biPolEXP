c74aabe8133a3a62de890b0eb5eb66a7379230ae
Mike Reddy <mike.reddy@southwales.ac.uk>
2024-10-16 15:13:57 +0100

    First Commit

---
+++ compile.bat
@@ -0,0 +1,20 @@
+@echo off
+
+set name="biPOlEXP"
+
+set path=%path%;..\bin\
+
+set CC65_HOME=TOOLS\
+
+cc65 -Oirs %name%.c --add-source
+ca65 crt0.s
+ca65 %name%.s -g
+
+ld65 -C nrom_32k_vert.cfg -o %name%.nes crt0.o %name%.o nes.lib -Ln %name%.labels --dbgfile %name%.dbg
+
+del *.o
+
+move /Y %name%.labels BUILD\ 
+move /Y %name%.s BUILD\ 
+move /Y %name%.nes BUILD\ 
+move /Y %name%.dbg BUILD\
